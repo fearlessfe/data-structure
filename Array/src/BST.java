0293df14805fdf6029d271ee7ec0d0087cefc68a
fearlessfe <505380967@qq.com>
2019-06-12 22:13:53 +0800

    递归实现二分搜索树的添加操作

--- Array/src/BST.java
+++ Array/src/BST.java
@@ -26,4 +26,40 @@ public class BST<T extends Comparable<T>> {
     public boolean isEmpty(){
         return size == 0;
     }
+
+    public void add(T e){
+        if(root == null){
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    private Node add(Node node, T e){
+        // 递归终止条件
+//        if(e.equals(node.e))
+//            return;
+//        else if(e.compareTo(node.e) < 0 && node.left == null){
+//            node.left = new Node(e);
+//            size++;
+//            return;
+//        }
+//        else if(e.compareTo(node.e) > 0 && node.right == null){
+//            node.right = new Node(e);
+//            size++;
+//            return;
+//        }
+        if(node == null){
+            size++;
+            return new Node(e);
+        }
+        // 递归调用
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
+    }
 }
